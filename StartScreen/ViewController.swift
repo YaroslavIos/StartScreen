5ee18f25e23f0c8bb6a526934e85ffca9c1eebd5
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-05-29 20:03:00 +0300

    Initial Commit

---
+++ StartScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StartScreen
+//
+//  Created by Ярослав Любиченко on 29.5.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
