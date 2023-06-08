94b63d3fa4827528dc6019031f011329d1bd1e9d
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-06-08 20:46:34 +0300

    Add a boarded button, set a ui design.

--- StartScreen/MainScreenViewController.swift
+++ StartScreen/MainScreenViewController.swift
@@ -13,17 +13,21 @@ class MainScreenViewController: UIViewController {
     let tintedButton = UIButton()
     let grayButton = UIButton()
     let plainButton = UIButton()
+    let borderedButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .black
+        
         setFilledButton()
         setTintedButton()
         setGrayButton()
+        setPlainButton()
+        setBorderedButton()
     }
     
-    // Mark: - Set a UI view of Filled Button.
+    // MARK: - Set a UI view of Filled Button.
     func setFilledButton() {
         view.addSubview(filledButton)
         
@@ -44,7 +48,7 @@ class MainScreenViewController: UIViewController {
         ])
     }
     
-    // Mark: - Set a UI view of Tinted Button.
+    // MARK: - Set a UI view of Tinted Button.
     func setTintedButton() {
         view.addSubview(tintedButton)
         
@@ -54,7 +58,6 @@ class MainScreenViewController: UIViewController {
         tintedButton.configuration?.imagePadding = 20
         tintedButton.setTitleColor(.white, for: .normal)
         tintedButton.backgroundColor = .systemBackground
-        tintedButton.configuration?.buttonSize = .small
         
         tintedButton.translatesAutoresizingMaskIntoConstraints = false
         
@@ -66,11 +69,16 @@ class MainScreenViewController: UIViewController {
         ])
     }
     
-    // Mark: - Set a UI view of Gray Button.
+    // MARK: - Set a UI view of Gray Button.
     func setGrayButton() {
         view.addSubview(grayButton)
         
         grayButton.configuration = .gray()
+        grayButton.setTitle("Gray", for: .normal)
+        grayButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        grayButton.configuration?.imagePadding = 20
+        grayButton.setTitleColor(.white, for: .normal)
+        grayButton.backgroundColor = .systemBackground
         
         grayButton.translatesAutoresizingMaskIntoConstraints = false
         
@@ -79,6 +87,50 @@ class MainScreenViewController: UIViewController {
             grayButton.heightAnchor.constraint(equalToConstant: 50),
             grayButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             grayButton.centerYAnchor.constraint(equalTo: tintedButton.bottomAnchor, constant: 40)
+        ])
+    }
+    
+    //MARK: - Set a UI view of Plain Button.
+    func setPlainButton() {
+        view.addSubview(plainButton)
+        
+        plainButton.configuration = .plain()
+        plainButton.setTitle("Plain", for: .normal)
+        plainButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        plainButton.configuration?.imagePadding = 20
+        plainButton.setTitleColor(.white, for: .normal)
+        plainButton.setTitleColor(.systemGreen, for: .highlighted)
+        plainButton.backgroundColor = .systemBackground
+        
+        plainButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            plainButton.widthAnchor.constraint(equalToConstant: 200),
+            plainButton.heightAnchor.constraint(equalToConstant: 50),
+            plainButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            plainButton.centerYAnchor.constraint(equalTo: grayButton.bottomAnchor, constant: 40)
+        ])
+    }
+    
+    // MARK: - Set a UI view of Bordered Button.
+    func setBorderedButton() {
+        view.addSubview(borderedButton)
+        
+        borderedButton.configuration = .bordered()
+        borderedButton.setTitle("Boarded", for: .normal)
+        borderedButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        borderedButton.configuration?.imagePadding = 20
+        borderedButton.layer.borderWidth = 2
+        borderedButton.layer.borderColor = .init(red: 255, green: 105, blue: 93, alpha: 1)
+        borderedButton.layer.cornerRadius = 16
+        
+        borderedButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            borderedButton.widthAnchor.constraint(equalToConstant: 200),
+            borderedButton.heightAnchor.constraint(equalToConstant: 50),
+            borderedButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            borderedButton.centerYAnchor.constraint(equalTo: plainButton.bottomAnchor, constant: 40)
         ])
     }
 }
